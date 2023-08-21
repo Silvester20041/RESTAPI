b2320b6b7e09a779cf3c4f3531c7126b58c3ce8b
Silvester20041 <108716503+Silvester20041@users.noreply.github.com>
2023-08-21 16:08:47 +0530

    Add files via upload

---
+++ day3/src/main/java/com/example/demo1/demo1.java
@@ -0,0 +1,13 @@
+package com.example.demo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class demo1 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(demo1.class, args);
+	}
+
+}
